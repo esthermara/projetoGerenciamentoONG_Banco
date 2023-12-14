c2633d3d7bffa5c12b4ee64c954ecc4274ac6eed
esthermara <esthermara1030@gmail.com>
2023-12-13 21:08:01 -0300

    ProjetoGerenciamentoONG: Script | Criação de DB e tables.

---
+++ MYSQL_CREATE_DB_TABLES_PROJETOGERENCIAMENTOONG.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE gerenciamentoong;
+
+USE gerenciamentoong;
+
+CREATE TABLE Usuario (
+  idUsuario int unsigned NOT NULL auto_increment,
+  nome varchar(50),
+  senha varchar(8),
+  competencia varchar(50),
+  PRIMARY KEY (idUsuario)
+);
+CREATE TABLE Asssociados (
+  idAssociados int unsigned NOT NULL auto_increment,
+  nome varchar(50),
+  endereco varchar (500),
+  email varchar (50),
+  telefone varchar (15),
+  dataAssociacao datetime,
+  PRIMARY KEY (idAssociados)
+);
+CREATE TABLE Oficinas (
+  idOficinas int unsigned NOT NULL auto_increment,
+  nomeOficina varchar (50),
+  diaSemana varchar (100),
+  horario datetime ,
+  professsor varchar (50),
+  PRIMARY KEY (idOficinas)
+);
+CREATE TABLE Estoque (
+  idEstoque int unsigned NOT NULL auto_increment,
+  numeroControle int,
+  descricao varchar (200),
+  quantidade int,
+  origem varchar (50),
+  PRIMARY KEY (idEstoque)
+);
+CREATE TABLE Faturamento (
+  idFaturamento int unsigned NOT NULL auto_increment,
+  descricao varchar (200),
+  origem varchar (50),
+  valorEntrada decimal,
+  valorSaida decimal,
+  totalGeral decimal,
+  PRIMARY KEY (idFaturamento)
+);
+
+
+
